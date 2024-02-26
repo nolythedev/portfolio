d2e16ab3a3529833fa56f5d2bd470a63ce2abca3
nolythedev <nolychigwedere@hotmail.co.uk>
2024-02-26 12:30:56 +0000

    Locomotive added, styling improved

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,50 @@
 console.log('Script has loaded');
 
 // Target the button 
-
+const bodyEl = document.body;
+const traitsEl = document.querySelector('#traits');
 const themeBtn = document.querySelector('#themeToggle');
-const lightMoon = '<i class="fa-regular fa-moon"></i>';
+const lightMoon = '<i class="fa-regular fa-sun"></i>';
 const darkMoon = '<i class="fa-solid fa-moon"></i>';
 
-console.log(document.body.children);
+let isDark = true; // Check if this should be set to true or false initially
+
+// On click check if light or dark and set to light or dark
+
+themeBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    // console.log('isDark before toggle:', isDark);
+    isDark = !isDark;
+    // console.log('isDark after toggle:', isDark);
+    if (isDark) {
+        bodyEl.classList.remove("light");
+        bodyEl.classList.add("dark"); 
+        themeBtn.innerHTML = darkMoon;
+        localStorage.setItem("darkMode", "Active");
+    } else {
+        bodyEl.classList.remove("dark");
+        bodyEl.classList.add("light"); 
+        themeBtn.innerHTML = lightMoon;
+        localStorage.setItem("darkMode", "Inactive");
+    }
+});
+
+const darkMode = localStorage.getItem("darkMode");
+
+if (darkMode === 'Active') {
+    isDark = true;
+    bodyEl.classList.remove("light");
+    bodyEl.classList.add("dark");
+    themeBtn.innerHTML = darkMoon;
+} else {
+    isDark = false;
+    bodyEl.classList.remove("dark");
+    bodyEl.classList.add("light");
+    themeBtn.innerHTML = lightMoon;
+}
 
 // Set initial text content of themeBtn 
-
-themeBtn.innerHTML = lightMoon;
 
 // Set initial css of button
 
 themeBtn.setAttribute('style', 'color: #121316; background-color: transparent; border: none; font-size: 25px; cursor: pointer;');
-
-// On click check if light or dark and set to light or dark
-
-function toggleTheme(event) {
-    event.preventDefault();
-
-    // Change button icon to light or dark 
-
-    // set theme of body to light or dark
-    if (themeBtn.innerHTML === lightMoon) {
-        themeBtn.innerHTML = darkMoon;
-        document.body.classList.remove('light');
-        document.body.classList.add('dark');
-    } else {
-        themeBtn.innerHTML = lightMoon;
-        document.body.classList.add('light');
-        document.body.classList.remove('dark');
-    }
-
-}
-
-themeBtn.addEventListener('click', toggleTheme);
